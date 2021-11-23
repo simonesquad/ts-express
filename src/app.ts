bce28581dd7ca91629a53a30b7192fd613819a8f
Simone Ballard <simone.a.ballard@gmail.com>
2021-11-23 17:14:13 -0500

    added some error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-const express = require('express');
+import express, { Request, Response, NextFunction } from 'express';
+
+import todoRoutes from './routes/todos';
 
 const app = express();
+
+app.use('/todos', todoRoutes);
+
+app.use((err: Error, req: express.Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message });
+});
 
 app.listen(3000);
